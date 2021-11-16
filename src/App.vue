973787ce6a3b35962ac7ba0aeb36f69914d8d068
Kevin Pratama <kevinpratama161000@mhs.mdp.ac.id>
2021-11-16 21:56:02 +0700

    Kevin Pratama Vue Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
 </template>
 
 <script>
@@ -22,5 +21,63 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: gray;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+.botnav {
+  overflow: hidden;
+  background-color: #000000;
+}
+.botnav p{
+  margin: 20px;
+  color: aliceblue;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,47 @@
 <template>
   <div class="hello">
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#about">New Car</a>
+      <a href="#about">Used Car</a>
+      <a href="#contact">Contact</a>
+      <div class="search-container">
+        <form action="/action_page.php">
+          <input type="text" placeholder="Search Car" name="search">
+          <button type="submit">Go</button>
+        </form>
+      </div>
+    </div>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Japan Cars</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><img src="https://awsimages.detik.net.id/community/media/visual/2020/03/22/e5590047-cc1c-4e33-b608-1752f46d5274_169.jpeg"></li>
+      <li><img src="https://t1-client.toyota-europe.com/images/toyota-logo.jpg"></li>
+      <li><img src="https://logosvector.net/wp-content/uploads/2014/10/honda-logo-vector-download.jpg"></li>
+     
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Italy Cars</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><img src="https://seeklogo.net/wp-content/uploads/2017/01/ferrari-logo.png"></li>
+      <li><img src="https://audimediacenter-a.akamaihd.net/system/production/media/28404/images/55eb0a5e2c5789e25ea1ff3ff521b414c3b70415/A160057_full.jpg?1582245524"></li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>Germany Cars</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><img src="https://luxe.tv/wp-content/uploads/2020/03/Logo-Audi-1.jpg"></li>
+      <li><img src="https://brandlogos.net/wp-content/uploads/2020/03/new-bmw-logo-2020-512x512.png"></li>
     </ul>
+    <h3>American Cars</h3>
+    <ul>
+      <li><img src="https://cdn.dribbble.com/users/469697/screenshots/6449432/ford_white_4x.jpg"></li>
+    </ul>
+    <div class="botnav">
+      <p>Collection Cars</p>
+      <p>Copyright 2020 | Hosting poweres by CloudFlare</p>
+      <p>Email</p>
+      <p>collectioncars@gmail.com</p>
+      <p>082177979924</p>
+    </div>
   </div>
 </template>
 
@@ -42,7 +57,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin: 100px 0 0;
+  font-size: 35px;
 }
 ul {
   list-style-type: none;
@@ -50,9 +66,13 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 50px 10px;
 }
 a {
   color: #42b983;
+}
+img{
+  width: 250px;
+  height: 160px;
 }
 </style>
